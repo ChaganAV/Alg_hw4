7974056958d13e4b325355ddd112574f872cc5ac
Alexandr <jwchaganav@gmail.com>
2023-06-21 16:36:28 +0400

    add начал

--- src/SimpleTree.java
+++ src/SimpleTree.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleTree {
     private Node root;
+    private enum Color{RED,BLACK};
     private class Node{
         private int value;
-        private List<Node> cheldren;
+        private Color color;
+        private Node leftChild;
+        private Node rightChild;
     }
+
+    public void add(int value){
+        Node node = new Node();
+        node.value = value;
+        node.color = Color.RED;
+        if(root == null){
+            node.color = Color.BLACK;
+            this.root = node;
+        } else {
+            if(root.value > value){
+                Node temp = root;
+                root = node;
+                root.color = Color.BLACK;
+            } else {
+            }
+
+        }
+    }
+
+
+
+
+
 }
