1bd22d7ae6f84076802285efae3b26aabd154a74
Alexandr <jwchaganav@gmail.com>
2023-06-22 16:40:42 +0400

    пример

--- src/SimpleTree.java
+++ src/SimpleTree.java
@@ -9,6 +9,32 @@ public class SimpleTree {
         private Color color;
         private Node leftChild;
         private Node rightChild;
+
+        @Override
+        public String toString() {
+            System.out.println(value);
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleTree{" +
+                "root=" + root +
+                '}';
+    }
+
+    private void print(Node root){
+        Node node = root;
+        while(node != null) {
+            System.out.println(node);
+            if (node.leftChild != null) {
+                print(node.leftChild);
+            }
+            if(node.rightChild != null){
+                print(node.rightChild);
+            }
+        }
     }
 
     public void add(int value){
@@ -19,18 +45,62 @@ public class SimpleTree {
             node.color = Color.BLACK;
             this.root = node;
         } else {
-            if(root.value > value){
-                Node temp = root;
-                root = node;
-                root.color = Color.BLACK;
-            } else {
+            if(root.value > node.value){
+                root.leftChild = addLeftNode(node,value);
+            }else{
+                root.rightChild = addRightNode(node,value);
             }
-
+        }
+    }
+    private Node addLeftNode(Node node, int value){
+        if(node.value > value){
+            if (node.leftChild == null){
+                Node newNode = new Node();
+                newNode.value = value;
+                newNode.color = Color.RED;
+                node.leftChild = newNode;
+                return node;
+            }else{
+                if(node.leftChild.value > value){
+                    return addLeftNode(node.leftChild,value);
+                }else{
+                    Node temp = node.leftChild;
+                    Node newNode = new Node();
+                    newNode.value = value;
+                    newNode.color = Color.RED;
+                    newNode.leftChild = temp;
+                    node.leftChild = newNode;
+                    return node;
+                }
+            }
+        }else{
+            return addRightNode(node,value);
         }
     }
 
-
-
-
-
+    private Node addRightNode(Node node, int value) {
+        if(node.value < value){
+            if (node.rightChild == null){
+                Node newNode = new Node();
+                newNode.value = value;
+                newNode.color = Color.BLACK;
+                node.rightChild = newNode;
+                return node;
+            }else{
+                if(node.leftChild.value > value){
+                    return addLeftNode(node.leftChild,value);
+                }else{
+                    Node temp = node.leftChild;
+                    Node newNode = new Node();
+                    newNode.value = value;
+                    newNode.color = Color.RED;
+                    newNode.leftChild = temp;
+                    node.leftChild = newNode;
+                    return node;
+                }
+            }
+        }else{
+            return addLeftNode(node,value);
+        }
+    }
 }
